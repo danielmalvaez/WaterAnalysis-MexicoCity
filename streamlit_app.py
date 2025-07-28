55c1b02c951c79c88f7cc664473fb895a12a630f
malvaaez <malvaez.axel@aries.iimas.unam.mx>
2025-07-27 22:31:22 -0600

    streamlit added

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+st.title('Escasez de Agua 💧 y cómo impacta las decisiones al momento de adquirir/rentar una propiedad en Ciudad de México 🏙️')
+st.markdown('_Prototype v0.1.0_')
